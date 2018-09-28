8549c74083659c5253c0979b70bcbf0b1e5c9c8d
sangramdesai123 <sangram.desai@somaiya.edu>
2018-09-28 21:29:32 +0530

    adding profile,register and search page

---
+++ profile.php
@@ -0,0 +1,103 @@
+<?php 
+	include_once 'partial/header.php'
+ ?>		
+
+<!--loop through data base for table-->
+
+
+		<section id="search" class="site-padding">
+		<h3 class="container count">Application Status for Internship</h3>
+			<div class="container">
+				<div class="row">
+				<!--Filter-->
+					<div class="col-sm-12">
+						<table id="customers">
+						  <tr>
+						 	<th>Date</th>
+						    <th>Company</th>
+						    <th>Applicaion Status</th>
+						    <th>Contact</th>
+						    <th>Country</th>
+						  </tr>
+						  <tr>
+						  	<td>16/02/2016</td>
+						    <td>Alfreds Futterkiste</td>
+						    <td>Applied</td>
+						    <td>Maria Anders</td>
+						    <td>Germany</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Berglunds snabbköp</td>
+						    <td>Applied</td>
+						    <td>Christina Berglund</td>
+						    <td>Sweden</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Centro comercial Moctezuma</td>
+						    <td>Applied</td>
+						    <td>Francisco Chang</td>
+						    <td>Mexico</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Ernst Handel</td>
+						    <td>Applied</td>
+						    <td>Roland Mendel</td>
+						    <td>Austria</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Island Trading</td>
+						    <td>Applied</td>
+						    <td>Helen Bennett</td>
+						    <td>UK</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Königlich Essen</td>
+						    <td>Applied</td>
+						    <td>Philip Cramer</td>
+						    <td>Germany</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Laughing Bacchus Winecellars</td>
+						    <td>Applied</td>
+						    <td>Yoshi Tannamuri</td>
+						    <td>Canada</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Magazzini Alimentari Riuniti</td>
+						    <td>Applied</td>
+						    <td>Giovanni Rovelli</td>
+						    <td>Italy</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>North/South</td>
+						    <td>Applied</td>
+						    <td>Simon Crowther</td>
+						    <td>UK</td>
+						  </tr>
+						  <tr>
+						    <td>16/02/2016</td>
+						    <td>Paris spécialités</td>
+						    <td>Applied</td>
+						    <td>Marie Bertrand</td>
+						    <td>France</td>
+						  </tr>
+						</table>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+		
+	
+
+<?php 
+	include_once 'partial/footer.php'
+ ?>		
---
+++ search.php
@@ -0,0 +1,52 @@
+<?php 
+	include_once 'partial/header.php'
+ ?>		
+
+		<section id="search" class="site-padding">
+		<h3 class="container count">230+ Internship</h3>
+			<div class="container">
+				<div class="row">
+				<!--Filter-->
+					<div class="col-sm-3 le">
+						<div >
+							<h4>Filters</h4>
+							<hr>
+						</div>
+						<h4>Category</h4>
+						<div class="search">
+						  <span <i class="fa fa-search"></span>
+						  <input placeholder="search">
+						</div>
+						<h4>Location</h4>
+						<div class="search">
+						  <span <i class="fa fa-search"></span>
+						  <input placeholder="search">
+						</div>
+						<h4>Max. duration (months)</h4>
+						<div class="search">
+						  <span <i class="fa fa-search"></span>
+						  <input placeholder="search">
+						</div>
+
+
+					</div>
+				<!--Filter Resl on righ-->
+					<div class="col-sm-9 rh">
+						<div class="result">
+							<h4>About Our Company</h4>
+							<p><h5>Location:Mumbai</h5></p>
+							
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<a href="#" class="btn btn-read-more mybtn">Read More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+
+
+
+<?php 
+	include_once 'partial/footer.php'
+ ?>		
